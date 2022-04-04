18e14d8fea4f7d6c716fb7a48b51b964f8e507f0
Ewig <valikallstars@gmail.com>
2022-04-04 19:54:29 +0300

    added form modul

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import burger from './modules/burger';
 import filter from './modules/filter';
 import accordion from './modules/accordion';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
 	'use strict';
@@ -12,4 +13,5 @@ window.addEventListener('DOMContentLoaded', () => {
 	accordion('.accordion-heading');
 	sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
 	sliders('.main-slider-item', 'vertical');
+	forms();
 });
