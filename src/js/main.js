b1f9c7fa7afbea4547e0e8aa02efec1c936107de
Ewig <valikallstars@gmail.com>
2022-04-04 21:28:52 +0300

    added text validator

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ import accordion from './modules/accordion';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
 	'use strict';
@@ -16,4 +17,6 @@ window.addEventListener('DOMContentLoaded', () => {
 	sliders('.main-slider-item', 'vertical');
 	forms();
 	mask('[name="phone"]');
+	checkTextInputs('[name="name"]');
+	checkTextInputs('[name="message"]');
 });
