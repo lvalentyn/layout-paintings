4c093b645f96ced73cb71e2dbc6d303d77bb5dc7
Ewig <valikallstars@gmail.com>
2022-04-04 21:22:01 +0300

    added phone mask for form

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import filter from './modules/filter';
 import accordion from './modules/accordion';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
+import mask from './modules/mask';
 
 window.addEventListener('DOMContentLoaded', () => {
 	'use strict';
@@ -14,4 +15,5 @@ window.addEventListener('DOMContentLoaded', () => {
 	sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
 	sliders('.main-slider-item', 'vertical');
 	forms();
+	mask('[name="phone"]');
 });
